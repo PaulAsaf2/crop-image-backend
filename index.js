f5cf7b45c99a627d89f5af996fda5d2285d83870
Pavel Asafov <pavelaof@gmail.com>
2024-04-09 22:08:46 +0300

    add local certificate

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer');
-const fs = require('fs');
 const path = require('path');
 // -------------------------------------
 const app = express();
 const PORT = 3000;
+const options = {
+  key: fs.readFileSync('./127.0.0.1-key.pem'),
+  cert: fs.readFileSync('./127.0.0.1.pem'),
+};
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const dest = '/root/crop-image-backend/uploads';
+    const dest = 'C:\\Users\\pavel\\repositories\\magnetto\\telegram-mini-app\\wallstring\\crop-image-divided\\backend\\uploads';
     cb(null, dest);
   },
   filename: function (req, file, cb) {
@@ -23,7 +28,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  const { code } = req.query;
+  // const { code } = req.query;
+  const code = '11111111111';
 
   if (code) {
     fs.writeFile('file.txt', code, (err) => {
@@ -37,7 +43,7 @@ app.use((req, res, next) => {
 
   next();
 })
-app.use('/api', express.static(path.join(__dirname, '../../var/www/crop-image-frontend')));
+app.use('/api', express.static(path.join(__dirname, '../frontend')));
 // -------------------------------------
 app.get('/api/get-code', (req, res) => {
   fs.readFile('file.txt', 'utf8', (err, data) => {
@@ -47,11 +53,11 @@ app.get('/api/get-code', (req, res) => {
   });
 });
 // -------------------------------------
-app.post('/api/submit', upload.single('image'), (req, res) => {
+app.post('/api/upload', upload.single('image'), (req, res) => {
   try {
     const { filename } = req.file;
     const filepath = path.join(__dirname, `uploads/${filename}`);
-    const file = fs.readFile(filepath, (err, data) => {
+    fs.readFile(filepath, (err, data) => {
       if (err) throw err;
       res.status(200).send({ message: req.file.filename })
     });
@@ -60,6 +66,7 @@ app.post('/api/submit', upload.single('image'), (req, res) => {
   }
 })
 // -------------------------------------
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+const server = https.createServer(options, app);
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Server is running on port ${PORT}`);
 })
